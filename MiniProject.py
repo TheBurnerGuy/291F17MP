7366a7d90d5a7014ff5ec9908e16937bce274fd6
TheBurnerGuy <henryweilo@hotmail.com>
2017-10-28 22:55:15 -0600

    Added login implementation
    Customer login, agent login, and customer registration are 90% complete.
    Invisible password input is missing.
    Note: Not tested at all!

--- MiniProject.py
+++ MiniProject.py
@@ -8,88 +8,122 @@ import Agent
 
 connection = None
 cursor = None
+lid = None #Login ID - general case between customer and agent
 
 def connect(path):
     global connection, cursor
 
     connection = sqlite3.connect(path)
     cursor = connection.cursor()
-    cursor.execute(' PRAGMA forteign_keys=ON; ')
+    cursor.execute(' PRAGMA foreign_keys=ON; ')
     connection.commit()
     return
 
-def define_tables():
-    global connection, cursor
+def customer_login():
+    #sets lid to cid upon successful login
+    #returns true or false based on success or failure of logging in
+    global connection, cursor, lid
+    
+    print "Enter your customer id:"
+    cid = input()
+    print "Enter your password:"
+    pwd = input() #TODO invisible implementation
+    
+    cursor.execute("SELECT * FROM customers WHERE cid = :cid AND pwd = :pwd;",{"cid": cid, "pwd": pwd})
+    if(len(cursor.fetchall()) == 1):
+        print "Login successful!"
+        lid = cid
+        return True
+    else:
+        print "Login failed."
+        return False
+    
+def agent_login():
+    #sets lid to aid upon successful login
+    #returns true or false based on success or failure of logging in
+    global connection, cursor, lid
+    
+    print "Enter your agent id:"
+    aid = input()
+    print "Enter your password:"
+    pwd = input() #TODO invisible implementation
+    
+    cursor.execute("SELECT * FROM agents WHERE aid = :aid AND pwd = :pwd;",{"aid": aid, "pwd": pwd})
+    if(len(cursor.fetchall()) == 1):
+        print "Login successful!"
+        lid = aid
+        return True
+    else:
+        print "Login failed. Invalid id or password."
+        return False
 
-    course_query=   '''
-                        CREATE TABLE course (
-                                    course_id INTEGER,
-                                    title TEXT,
-                                    seats_available INTEGER,
-                                    PRIMARY KEY (course_id)
-                                    );
-                    '''
-
-    student_query=  '''
-                        CREATE TABLE student (
-                                    student_id INTEGER,
-                                    name TEXT,
-                                    PRIMARY KEY (student_id)
-                                    );
-                    '''
-
-    enroll_query= '''
-                    CREATE TABLE enroll (
-                                student_id INTEGER,
-                                course_id INTEGER,
-                                enroll_date DATE,
-                                grade TEXT,
-                                PRIMARY KEY (student_id, course_id),
-                                FOREIGN KEY(student_id) REFERENCES student(student_id),
-                                FOREIGN KEY(course_id) REFERENCES course(course_id)
-                                );
-                '''
-
-    cursor.execute(course_query)
-    cursor.execute(student_query)
-    cursor.execute(enroll_query)
+def customer_register():
+    #Implementation of registering customer into database
+    global connection, cursor, lid
+    
+    print "Enter your name:"
+    name = input()
+    print "Enter your address:"
+    address = input()
+    
+    while(True):
+        print "Enter an unique customer id:"
+        cid = input()
+        cursor.execute("SELECT * FROM customers WHERE cid = :cid;",{"cid": cid})
+        if(len(cursor.fetchall()) == 0):
+            break
+        else:
+            print("{0} is not a unique customer id.".format(cid))
+    
+    print "Enter a password:"
+    pwd = input() #TODO Invisible implementation
+    
+    cursor.execute("INSERT INTO customers VALUES (:cid, :name, :address, :pwd);", {"cid": cid, "name": name, "address": address, "pwd": pwd})
     connection.commit()
-
+    print "Registration successful!"
+    
     return
 
-def insert_data():
-    global connection, cursor
-
-    insert_courses = '''
-                        INSERT INTO course(course_id, title, seats_available) VALUES
-                            (1, 'CMPUT 291', 200),
-                            (2, 'CMPUT 391', 100),
-                            (3, 'CMPUT 101', 300);
-                     '''
-
-    insert_students =  '''
-                        INSERT INTO student(student_id, name) VALUES
-                                (1509106, 'Saeed'),
-                                (1409106, 'Alex'),
-                                (1609106, 'Mike');
-                       '''
-
-    cursor.execute(insert_courses)
-    cursor.execute(insert_students)
-    connection.commit()
+def customer():
+    #Insert implementation of customer
+    return
+    
+def agent():
+    #Insert implementation of agent
     return
 
 def main():
-    global connection, cursor
+    global connection, cursor, lid
 
     path="./register.db"
     connect(path)
-    connection.create_function('GPA', 1, GPA)
+    #connection.create_function('GPA', 1, GPA)
 
-    define_tables()
-    insert_data()
+    #tables should be defined already in register.db
+    #data should be inserted already in register.db
     
-
+    #Login implementation
+    active = True
+    while(active):
+        try:
+            print "1. Customer Login\n2. Agent Login\n3. Customer Register\n4. Exit"
+            answer = int(input())
+            if(answer == 1):
+                if(customer_login()):
+                    customer()
+            elif(answer == 2):
+                if(agent_login()):
+                    agent()
+            elif(answer == 3):
+                customer_register()
+                customer()
+            elif(answer == 4):
+                active = False
+            else:
+                print "Invalid input. Try again."
+        except ValueError:
+            #Taken from https://stackoverflow.com/questions/8075877/converting-string-to-int-using-try-except-in-python
+            print 'Invalid input. Try again.'
 
     connection.commit()
     connection.close()
