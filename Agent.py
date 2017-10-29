eee9623f77edfca1c5cba3a794c68a062cc4d6b6
TheBurnerGuy <henryweilo@hotmail.com>
2017-10-29 14:49:56 -0600

    setup framework for setup deliveries function
    title, not complete at all

--- Agent.py
+++ Agent.py
@@ -1,1 +1,12 @@
 #Insert Agent methods here
+import numpy.random as rnd
+
+def setup(connection, cursor):
+    #Generate a unique trackingNo
+    trackingNo = rnd.randint(1,10000) #placeholder
+    
+    #List orders
+    
+    #Pick a order
+    
+    #Change pick up time (or not)
