2899b46d3f54d0918a748e3d27f2fe8b41660222
Henry Lo <henrywei@cs.ualberta.ca>
2017-11-06 12:33:17 -0700

    first agent function is functional

--- Agent.py
+++ Agent.py
@@ -32,18 +32,13 @@ def setup(connection, cursor):
     # Format: 5-digits, start with 10000
     
     cursor.execute("SELECT max(trackingno) FROM deliveries;")
-    trackingno = cursor.fetchone() + 1
+    trackingno = cursor.fetchone()[0] + 1
     if trackingno == 1:
         trackingno = 10001
     
     #List orders - orders that aren't already in a delivery
     query = '''SELECT oid
     FROM orders
-    
-    EXCEPT
-    
-    SELECT oid
-    FROM deliveries
     '''
     cursor.execute(query)
     orderList = cursor.fetchall()
@@ -58,14 +53,14 @@ def setup(connection, cursor):
         FROM orders
         WHERE oid = :oid
         '''
-        cursor.execute(query, {"oid": orderList[i]})
+        cursor.execute(query, {"oid": orderList[i][0]})
         result = cursor.fetchone()
-        print (i + ".\t" + result[0] + "\t" + result[1] + "\t" + result[2] + "\t" + result[3] )   
+        print (str(i) + ".\t" + str(result[0]) + "\t" + str(result[1]) + "\t" + str(result[2]) + "\t" + str(result[3]) )   
     
     #Pick a order
-    print ("Select an order using index to add to the delivery. Input 'q' to finish setup for this delivery.")
     active = True
     while active:
+        print ("Select an order using index to add to the delivery. Input 'q' to finish setup for this delivery.")
         choice = input()
         if choice == 'q':
             active = False
@@ -79,11 +74,11 @@ def setup(connection, cursor):
                         #Taken from https://stackoverflow.com/questions/12672629/python-how-to-convert-string-into-datetime
                         pickUpTime = datetime.datetime.strptime(pickUpTime, "%Y-%m-%d %H:%M:%S")
                         query = '''INSERT INTO deliveries VALUES (:trackingno, :oid, :pickUpTime, null);'''
-                        cursor.execute(query, {"trackingno": trackingno, "oid": orderList[choice], "pickUpTime": pickUpTime})
+                        cursor.execute(query, {"trackingno": trackingno, "oid": orderList[choice][0], "pickUpTime": pickUpTime})
                     else:
                         #Insert without pickup time
                         query = '''INSERT INTO deliveries VALUES (:trackingno, :oid, null, null);'''
-                        cursor.execute(query, {"trackingno": trackingno, "oid": orderList[choice]})
+                        cursor.execute(query, {"trackingno": trackingno, "oid": orderList[choice][0]})
                     connection.commit()
                     print ("Order added to delivery!")
             except ValueError:
